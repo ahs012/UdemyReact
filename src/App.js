58dbbbceeb63a3a70522edbe6af17b1881a79c58
Smoss <ahs012@gmail.com>
2019-08-30 18:18:47 -0400

    Corrected React Syntax

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/html'
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello, I am a React App Review</h1>
+       <Header/>
     </div>
   );
 }
